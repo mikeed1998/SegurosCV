d1d00cf6ced506d0a2c8c60928c1741b3c6f3434
Michael Eduardo Sandoal Pérez <49420226+mikeed1998@users.noreply.github.com>
2023-10-09 16:44:45 -0600

    solo falta el responsive de detalle

---
+++ detalle.php
@@ -0,0 +1,244 @@
+<?php
+    require('includes/layout.php');
+?>
+
+<?=$headGNRL;?>
+<?=$header;?>
+
+<div class="container-fluid pb-5" style="background-color: white;">
+        <div class="row" style="background-color: black;">
+            <div class="col-6 position-relative " style="
+                background-image: url('img/insurance1.jpg');
+                background-size: cover;
+                background-repeat: no-repeat;
+            ">
+                <div class="col-8 d-flex  position-absolute top-50 start-50 translate-middle">
+                    <img src="img/tienda/producto001.png" alt="" class="img-fluid w-100"> 
+                </div>
+            </div>
+            <div class="col-6 px-5 py-5 text-white">
+                <div class="row">
+                    <div class="col fs-1 text-start fw-bolder">
+                        Seguro - X
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col pb-3 fs-1 text-start fw-light">
+                        Tipo - X
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 border-top border-dark"></div>
+                </div>
+                <div class="row">
+                    <div class="col-9 py-4 text-white-50 text-start" style="text-align: justify; line-height: 1.2;">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, exercitationem.
+                        <br>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro enim eligendi dignissimos ut voluptate voluptas, beatae voluptatem itaque assumenda repellendus. At corrupti sed iste laboriosam accusantium. Deleniti repellendus molestias voluptas!
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-9 border-top border-dark"></div>
+                </div>
+                <div class="row">
+                    <div class="col-9 border-top border-dark"></div>
+                </div>
+                <div class="row">
+                    <div class="col py-2 fs-2">
+                        <div class="row">
+                            <div class="col-3 fw-light mt-2">
+                                Características
+                            </div>
+                            <div class="col-6 fs-5">
+                                <div class="dropdown">
+                                    <button class="btn btn-outline text-white dropdown-toggle fs-2 fw-light" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <div class="row">
+                                            <div class="col-8 text-end">
+                                                Uno
+                                            </div>
+                                            <div class="col-4 text-start">
+                                                <div class="row mt-1">
+                                                    <div class="col py-1 d-flex align-content-bottom">
+                                                        <img src="img/detalle/up.png" alt="" class="img-fluid">
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col py-1 d-flex align-content-top">
+                                                        <img src="img/detalle/down.png" alt="" class="img-fluid">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </button>
+                                    <ul class="dropdown-menu border border-white fs-4" style="background-color: black;">
+                                        <li><a class="dropdown-item text-white" style="background-color: black;" href="#">Dos</a></li>
+                                        <li><a class="dropdown-item text-white" style="background-color: black;" href="#">Tres</a></li>
+                                        <li><a class="dropdown-item text-white" style="background-color: black;" href="#">Cuatro</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-9 border-top border-dark"></div>
+                </div>
+                <div class="row">
+                    <div class="col-10 py-5">
+                        <div class="row">
+                            
+                            <div class="col-6">
+                                <a href="#/" class="btn btn-outline w-100 border-white text-white rounded-pill fs-5 py-4 fw-light" style="text-decoration: none;">Aplicar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col pt-5 display-3 text-center">
+                Relacionados
+            </div>
+        </div>
+        <div class="row">
+            <div class="slider-relacionados">
+                <div class="col-4 mt-5 ps-3 pe-3 mx-auto py-3 tipo tipo-1">
+                        <div class="card carta-seguro">
+                            <img src="img/logo.jpg" alt="" class="img-fluid w-auto p-3 mx-auto">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col text-center fs-3">
+                                        Seguro X
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col text-center fs-5 text-secondary">
+                                        Tipo X
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col py-2 text-center fs-5 text-secondary">
+                                        <a href="detalle.php" class="btn btn-outline border border-info">Ver detalle</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4 mt-5 ps-3 pe-3 mx-auto py-3 tipo tipo-2">
+                        <div class="card carta-seguro">
+                            <img src="img/logo.jpg" alt="" class="img-fluid w-auto p-3 mx-auto">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col text-center fs-3">
+                                        Seguro X
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col text-center fs-5 text-secondary">
+                                        Tipo X
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col py-2 text-center fs-5 text-secondary">
+                                        <a href="detalle.php" class="btn btn-outline border border-info">Ver detalle</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4 mt-5 ps-3 pe-3 mx-auto py-3 tipo tipo-3">
+                        <div class="card carta-seguro">
+                            <img src="img/logo.jpg" alt="" class="img-fluid w-auto p-3 mx-auto">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col text-center fs-3">
+                                        Seguro X
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col text-center fs-5 text-secondary">
+                                        Tipo X
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col py-2 text-center fs-5 text-secondary">
+                                        <a href="detalle.php" class="btn btn-outline border border-info">Ver detalle</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4 mt-5 ps-3 pe-3 mx-auto py-3 tipo tipo-3">
+                        <div class="card carta-seguro">
+                            <img src="img/logo.jpg" alt="" class="img-fluid w-auto p-3 mx-auto">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col text-center fs-3">
+                                        Seguro X
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col text-center fs-5 text-secondary">
+                                        Tipo X
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col py-2 text-center fs-5 text-secondary">
+                                        <a href="detalle.php" class="btn btn-outline border border-info">Ver detalle</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<script>
+     $('.slider-relacionados').slick({
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        speed: 500,
+        prevArrow: false,
+        nextArrow: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        ]
+    });
+			
+</script>
+
+<?=$footerGNRL;?>
+
+
+
+
---
+++ seguros.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-10-2023 a las 22:42:29
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `seguros`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nombres` varchar(255) NOT NULL,
+  `apellidos` varchar(255) NOT NULL,
+  `correo` varchar(255) NOT NULL,
+  `pasword` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `token_verificado` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `nombres`, `apellidos`, `correo`, `pasword`, `token`, `token_verificado`) VALUES
+(2, 'ddssds', 'fdfdfd', 'mikeed1998@gmail.com', 'MTIzNDU2Nzg=', 'd5084bdae415cce05e585b78322b2b69', 1),
+(3, 'dato2', 'apes2', 'michaelwozial@gmail.com', 'MTIzNDU2Nzg=', 'e6cb0b3edea951d200ad0cefbeee3b94', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
